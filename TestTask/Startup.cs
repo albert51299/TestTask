3aa690222fbd6bbb88b6bdf9498bac5ee9f6ee53
Albert <albert51299@gmail.com>
2019-09-21 14:06:35 +0500

    Add project files

---
+++ TestTask/Startup.cs
@@ -0,0 +1,50 @@
+﻿using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
+using TestTask.Models;
+using TestTask.Models.DataManager;
+using TestTask.Models.Repository;
+
+namespace TestTask {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
+            services.AddDbContext<VaccinationsContext>(o => o.UseSqlServer(Configuration["ConnectionString:DefaultConnection"]));
+            services.AddScoped<IDataRepository<Vaccination>, VaccinationManager>();
+            services.AddScoped<IDataRepository<Patient>, PatientManager>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            /*if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            else {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }*/
+            app.UseReact(config => { });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ TestTask/wwwroot/js/index.jsx
@@ -0,0 +1,18 @@
+﻿class Content extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                content
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Content />,
+    document.getElementById("index")
+);
