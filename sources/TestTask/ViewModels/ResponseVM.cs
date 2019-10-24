a36a0fead0aa5abbb939c113133c875f997b5e52
Альберт <albert51299@gmail.com>
2019-10-24 11:43:31 +0500

    feat(back): Добавлена модель ответа сервиса #7

---
+++ sources/TestTask/ViewModels/ResponseVM.cs
@@ -0,0 +1,27 @@
+﻿namespace TestTask.ViewModels {
+    /// <summary>
+    /// Ответ сервиса.
+    /// </summary>
+    public class ResponseVM {
+        /// <summary>
+        /// Успешно ли прошло выполнение запроса.
+        /// </summary>
+        public bool IsSuccess { get; set; }
+        /// <summary>
+        /// Сообщение об ошибке.
+        /// </summary>
+        public string ErrorMessage { get; set; }
+        /// <summary>
+        /// Код, с которым завершился запрос.
+        /// </summary>
+        public int StatusCode { get; set; }
+        /// <summary>
+        /// Идентификатор записи.
+        /// </summary>
+        public string Result { get; set; }
+        /// <summary>
+        /// Отправляемые данные (могут отсутствовать).
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
